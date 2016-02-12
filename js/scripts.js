1e791b8336ac3f4607df4f8bec04aa779fbd898d
Najm Sheikh <najm97@gmail.com>
2016-02-12 00:05:51 -0500

    Added ripples to all pages

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,54 @@ $(function() {
         loop: true
     });
 
+    $('#infoBtn').click(function(e) {
+        goGold();
+        var touch = {
+            pageX: e.pageX,
+            pageY: e.pageY,
+            identifier: 'ripple_' + Math.random() + '__ripple'
+        }
+        addTouch(touch, 'gold');
+        setTimeout(dropTouch(touch), 100)
+        switchPage('info');
+    });
+
+    $('#faqBtn').click(function(e) {
+        goWhite();
+        var touch = {
+            pageX: e.pageX,
+            pageY: e.pageY,
+            identifier: 'ripple_' + Math.random() + '__ripple'
+        }
+        addTouch(touch, 'white');
+        setTimeout(dropTouch(touch), 100)
+        switchPage('faq');
+    });
+
+    $('#scheduleBtn').click(function(e) {
+        goGold();
+        var touch = {
+            pageX: e.pageX,
+            pageY: e.pageY,
+            identifier: 'ripple_' + Math.random() + '__ripple'
+        }
+        addTouch(touch, 'gold');
+        setTimeout(dropTouch(touch), 100)
+        switchPage('schedule');
+    });
+
+    $('#sponsorsBtn').click(function(e) {
+        goWhite();
+        var touch = {
+            pageX: e.pageX,
+            pageY: e.pageY,
+            identifier: 'ripple_' + Math.random() + '__ripple'
+        }
+        addTouch(touch, 'white');
+        setTimeout(dropTouch(touch), 100)
+        switchPage('sponsors');
+    });
+
     $('#attendBtn').click(function(e) {
         $(this).removeClass('qhgold').addClass('qhgold-text white selected');
         goGold();
@@ -18,11 +66,7 @@ $(function() {
         }
         addTouch(touch, 'gold');
         setTimeout(dropTouch(touch), 100)
-        $('.welcome-message').fadeOut();
-        setTimeout(function(e) {
-            $('.attend-page').fadeIn();
-            $('#registrationForm').focus()
-        }, 400)
+        switchPage('attend');
     })
 });
 
@@ -85,4 +129,31 @@ function goGold() {
     $('#attendBtn').removeClass('qhgold').addClass('qhgold-text white');
     $('.link').css('color', '#ffffff');
     $('.link').removeClass('goldLink').addClass('whiteLink');
+}
+
+function switchPage(page) {
+	$('.page').fadeOut();
+    if (page === 'info') {
+        setTimeout(function(e) {
+            $('.info-page').fadeIn();
+        }, 400)
+    } else if (page === 'faq') {
+        setTimeout(function(e) {
+            $('.faq-page').fadeIn();
+        }, 400)
+    } else if (page === 'schedule') {
+        setTimeout(function(e) {
+            $('.schedule-page').fadeIn();
+        }, 400)
+    } else if (page == 'sponsors') {
+    	console.log('shit works')
+        setTimeout(function(e) {
+            $('.sponsors-page').fadeIn();
+        }, 400)
+    } else {
+        setTimeout(function(e) {
+            $('.attend-page').fadeIn();
+            $('#registrationForm').focus()
+        }, 400)
+    }
 }
